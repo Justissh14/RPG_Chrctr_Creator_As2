7877fd1a57cd8f34c6b2447c97e5aded750562a3
Justissh14 <justissholden14@gmail.com>
2024-10-23 22:01:36 -0400

    more classes and function
    added 2 new classes and another functions which now leaves me the scirpt to continue on and debugg/ add on anything left to finnish the small projec time working on as well as another do while loop and switch case with various exit options.

--- Source.cpp
+++ Source.cpp
@@ -11,22 +11,37 @@ using namespace std::chrono;
 using std::cout;
 using std::cin;
 using std::endl;
-
+// Character class
 class Character {
 public:
     string name;
     int age;
-    Character(string n, int a) : name(n), age(a) {} // fixed constructor
+    Character(string n, int a) : name(n), age(a) {} // still needa fix the constructor
     Character() = default;
 };
-
-class Class {
-
+// Lineage, class. Holds Lineage info 
+class Lineage {
 public:
+    int Bagger;
+    int Noble;
+    int Monster;
+    int Assassin;
 
+    Lineage(int B, int N, int M, int A) : Bagger(B), Noble(N), Monster(M), Assassin(A) {}
+};
+// Player Class, class. Hold the Class list/ info
+class Class {
+public:
+    int Wizard;
+    int Barbarian;
+    int Monk;
+    int Rogue;
+    Class(int W, int B, int M, int R) : Wizard(W), Barbarian(B), Monk(M), Rogue(R) {}
+
+    Class() = default;
 };
 
-// Menu Functions
+// Main Menu [1] Function
 void LineageMenu() { // this is the main display menu that the player will see and then be prompted to input a number that correlates to the lineage listed below
     cout << "__________" << endl;
     cout << "[1]Bagger " << endl;
@@ -36,6 +51,16 @@ void LineageMenu() { // this is the main display menu that the player will see a
     cout << "[5]-Quit-" << endl;
     cout << "-----------" << endl;
 };
+// Sub Menu [2] Function
+void ClassMenu() {
+    cout << "__________" << endl;
+    cout << "[1]Wizard " << endl;
+    cout << "[2]Barbarian " << endl;
+    cout << "[3]Monk " << endl;
+    cout << "[4]Rogue " << endl;
+    cout << "[5]<Back " << endl;
+    cout << "----------" << endl;
+}
 
 int Exit_Answr;
 
@@ -59,12 +84,17 @@ int main() {
     /* std::srand(std::time(0));
        cout << (std::rand() % 8) + 1 << endl; */ // random number between 1-8
 
-    int choice;
+    // Exit loop choices
+    int choice,choice2 = 0;
+    
+
+
     do {
         cin >> choice;
         switch (choice) {
         case 1:
             cout << "\n Ah yes, a poor Bagger indeed\n";
+            ClassMenu();
             break;
         case 2:
             cout << "\n Noble indeed, great things are to be expected of you\n";
@@ -73,7 +103,7 @@ int main() {
             cout << "\n You're a Monster!, selfish and ugly yet dangerous!\n";
             break;
         case 4:
-            cout << "\n An Assassin is sneaky and agile, use this speed wisely\n";
+            cout << "\n An Assassin is sneaky and agile, use this info wisely\n";
             break;
         case 5:
             cout << "\nAre you sure you want to Quit?\n";
@@ -96,6 +126,45 @@ int main() {
             cout << "Invalid option, please try again" << endl;
         }
     } while (choice != 5);
+    ClassMenu();
+   int Exit_Answr = 0;
+    
+    do {
+        cin >> choice2;
+
+        switch (choice2) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            cout << "\nAre you sure you want to Quit?\n";
+            cout << "([1]y / [2]n)?" << endl;
+            cin >> Exit_Answr;
+            if (Exit_Answr == 1) {
+                cout << "\n Exiting game, goodbye!\n ";
+                return 0;
+            }
+            else if (Exit_Answr == 2) {
+                cout << "\n You have chosen to stay\n ";
+                std::this_thread::sleep_for(chrono::seconds(2));
+                continue;
+            }
+            else {
+                cout << "Invalid option, please try again" << endl;
+            }
+            break;
+        default:
+            cout << "Invalid option, please try again" << endl;
+
+        }
+        
+
+    } while (choice2 != 5);
 
     return 0;
 }
